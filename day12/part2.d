bce9fcbc23d467c25d16325a6fc5d8bb1488edf7
Azi Hassan <azi.hassan@live.fr>
2022-12-12 22:02:17 +0100

    [master] Day 12: Hill Climbing Algorithm

---
+++ day12/part2.d
@@ -0,0 +1,165 @@
+import std;
+
+void main()
+{
+    char[][] map = stdin.byLine.map!strip.map!dup.array;
+    Point[] start;
+    Point end;
+    foreach(y; 0 .. map.length)
+    {
+        foreach(x; 0 .. map[y].length)
+        {
+            Point current = Point(cast(int) x, cast(int) y);
+            if(map.cellAt(current) == 'S' || map.cellAt(current) == 'a')
+            {
+                start ~= current;
+            }
+            else if(map.cellAt(current) == 'E')
+            {
+                end = current;
+                map[y][x] = 'z';
+            }
+        }
+    }
+    start.map!(s => map.findMinimalDistance(s, end)).filter!(length => length > 0).minElement().writeln();
+}
+
+ulong findMinimalDistance(char[][] map, Point start, Point end)
+{
+    bool[Point] visited;
+    Point[Point] parents;
+    DList!Point queue;
+    queue ~= start;
+    visited[start] = true;
+    while(!queue.empty())
+    {
+        Point current = queue.front();
+        queue.removeFront();
+        if(current == end)
+        {
+            auto path = parents.buildPath(end);
+            return path.length - 1;
+        }
+
+        foreach(neighbor; map.neighbors(current))
+        {
+            if((neighbor in visited) == null)
+            {
+                visited[neighbor] = true;
+                queue ~= neighbor;
+                parents[neighbor] = current;
+            }
+        }
+    }
+    return 0;
+}
+
+Point[] buildPath(Point[Point] parents, Point end)
+{
+    Point[] sequential = [end];
+    Point current = end;
+    while(current in parents)
+    {
+        current = parents[current];
+        sequential ~= current;
+    }
+    sequential.reverse();
+    return sequential;
+}
+
+void print(char[][] map, Point[] path)
+{
+    path.map!(p => p.to!string ~ "(" ~ map.cellAt(p) ~ ")").joiner(", ").writeln();
+    writeln("Total steps : ", path.length);
+}
+
+Point[] neighbors(char[][] map, Point cell)
+{
+    Point[] adjescent = [
+        Point(-1, 0),
+        Point(+1, 0),
+        Point(0, -1),
+        Point(0, +1),
+    ];
+    Point[] neighbors;
+    foreach(delta; adjescent)
+    {
+        Point neighbor = delta + cell;
+        if(!map.withinBounds(neighbor))
+        {
+            continue;
+        }
+        int difference = map.cellAt(cell) - map.cellAt(neighbor);
+        if(difference >= -1 || map.cellAt(cell) == 'S')
+        {
+            neighbors ~= neighbor;
+        }
+    }
+    return neighbors;
+}
+
+char cellAt(char[][] map, Point cell)
+{
+    return map[cell.y][cell.x];
+}
+
+bool withinBounds(char[][] map, Point cell)
+{
+    return 0 <= cell.x && cell.x < map[0].length && 0 <= cell.y && cell.y < map.length;
+}
+
+unittest
+{
+    char[][] map = [
+        "Sabqponm".dup,
+        "abcryxxl".dup,
+        "accszzxk".dup,
+        "acctuvwj".dup,
+        "abdefghi".dup
+    ];
+
+    Point[] start = [
+        Point(0, 0),
+        Point(0, 1),
+        Point(0, 2),
+        Point(0, 3),
+        Point(0, 4),
+        Point(1, 0),
+    ];
+
+    ulong result = start
+        .map!(s => map.findMinimalDistance(s, Point(5, 2)))
+        .filter!(length => length > 0)
+        .minElement;
+    assert(result == 29);
+}
+
+struct Point
+{
+    int x;
+    int y;
+
+    Point opBinary(string op)(Point other) const if(op == "+" || op == "-")
+    {
+        return Point(
+            mixin("x " ~ op ~ " other.x"),
+            mixin("y " ~ op ~ " other.y")
+        );
+    }
+
+    bool opEquals(const Point other) const
+    {
+        return other.x == x && other.y == y;
+    }
+
+    string toString() const
+    {
+        return format!"[%d, %d]"(x, y);
+    }
+
+    size_t toHash() const
+    {
+        return toString().hashOf();
+    }
+}
+
